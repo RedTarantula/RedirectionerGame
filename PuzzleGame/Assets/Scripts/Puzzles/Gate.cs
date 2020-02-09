bf1c361a197bd6e260473af750d285e4eba61e75
RedTarantula <joaopedrothmann@hotmail.com>
2020-02-09 19:35:07 -0300

    Button and gate

---
+++ PuzzleGame/Assets/Scripts/Puzzles/Gate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public bool locked = true;
+
+    public void Start()
+    {
+        UpdateGate();
+    }
+
+    public void ToggleGate()
+    {
+        locked = !locked;
+        UpdateGate();
+
+    }
+
+    public void UpdateGate()
+    {
+        if(locked)
+        {
+            GetComponentInChildren<SpriteRenderer>().color = Color.magenta;
+            gameObject.layer = 9;
+        }
+        else
+        {
+            GetComponentInChildren<SpriteRenderer>().color = Color.grey;
+            gameObject.layer = 12;
+        }
+    }
+
+}
---
+++ PuzzleGame/Assets/Scripts/Puzzles/GateButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateButton : MonoBehaviour
+{
+    public List<Gate> gates = new List<Gate>();
+    public bool broken = false;
+
+    public void BreakButton()
+    {
+        broken = false;
+    }
+
+    public void ToggleButton()
+    {
+        if(gates.Count <= 0)
+        {
+            Debug.LogError("No gates set for button");
+            return;
+        }
+
+        if (!broken)
+        {
+            foreach (Gate g in gates)
+            {
+                g.ToggleGate();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ToggleButton();
+        }
+        else if (collision.CompareTag("CannonBall"))
+        {
+            BreakButton();
+        }
+    }
+
+}
